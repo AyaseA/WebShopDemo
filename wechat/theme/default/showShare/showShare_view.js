b57a65dd02ebadd7bf41c4ff2f5d175b5b0736ac
lc <617650056@qq.com>
2017-06-08 19:06:16 +0800

    share

--- wechat/theme/default/showShare/showShare_view.js
+++ wechat/theme/default/showShare/showShare_view.js
@@ -12,19 +12,70 @@ $(function() {
 
             return false;
         }
-
-        layer.prompt({
-            formType: 3,
-            // value: '初始值',
-            placeHolder: 'wertyuio',
-            title: '请输入手机验证码领取优惠',
-            // area: ['800px', '350px'] //自定义文本域宽高
-        }, function(value, index, elem) {
-            alert(value); //得到value
-            layer.close(index);
+        var param = { Mobile: mobile };
+        $$.post('CSL/Login/HasUserMobile', param, function(data) {
+            if (data.Status > 0) {
+                sendVerify();
+            } else if (data.Status == 0) {
+                layer.msg('用户已经注册!');
+            }
+        }, function(error) {
+            layer.msg('请求失败,请检查网络设置!');
         });
 
+        function sendVerify() {
+            var params = { Mobile: mobile, Type: 6, PType: 0 };
+            $$.post('Product/Info/SendVerifiedCode', params, function(data) {
+                if (data.Status == 0) {
+                    showPrompt();
+                } else {
+                    layer.msg('网络异常');
+                }
+            }, function(error) {
+                layer.msg('请求失败,请检查网络设置!');
+            });
+        }
 
-        REFormObj.submit();
+        function regiestUser(phoneNum, verify,index) {
+
+
+            var params = { Mobile: mobile, SessionID: 1, RegisterFrom: 2, RegisterCont: 1, VC:verify };
+debugger
+            $$.post('CSL/Login/HasUserMobile', params, function(data) {
+                console.log(1);
+                if (data.Status == 0) {
+                console.log(2);
+
+                    layer.msg('注册成功');
+                    layer.close(index);
+                } else if (-3 == data.Status) {
+                    
+                    layer.msg('验证码输入错误,请重新输入');
+                }else{
+
+                }
+            }, function(error) {
+
+                            layer.msg('请求失败,请检查网络设置!');
+            });
+        }
+
+
+        // post: function(url, data, succfunc, errfunc) 
+        function showPrompt() {
+            layer.prompt({
+                formType: 3,
+                // value: '初始值
+                title: '请输入手机验证码领取优惠',
+                // area: ['800px', '350px'] //自定义文本域宽高
+            }, function(value, index, elem) {
+                alert(value);
+                regiestUser(mobile,value,index);
+            });
+        }
+
+
+
+        // REFormObj.submit();
     }
 });

f4a41c994ed8b82b3b770dd92d5d848b2eb397f2
lc <617650056@qq.com>
2017-06-08 15:14:15 +0800

    share

--- wechat/theme/default/showShare/showShare_view.js
+++ wechat/theme/default/showShare/showShare_view.js
@@ -1,5 +1,5 @@
 $(function() {
- var REFormObj = document.getElementById('REForm');
+    var REFormObj = document.getElementById('REForm');
     var submitREObj = document.getElementById('submitRE');
 
     submitREObj.onclick = function() {
@@ -8,9 +8,22 @@ $(function() {
 
         var mobileReg = /^1\d{10}$/;
         if (!mobileReg.test(mobile)) {
-            alert('手机格式不正确');
+            layer.msg('请输入正确地手机号！');
+
             return false;
         }
+
+        layer.prompt({
+            formType: 3,
+            // value: '初始值',
+            placeHolder: 'wertyuio',
+            title: '请输入手机验证码领取优惠',
+            // area: ['800px', '350px'] //自定义文本域宽高
+        }, function(value, index, elem) {
+            alert(value); //得到value
+            layer.close(index);
+        });
+
 
         REFormObj.submit();
     }

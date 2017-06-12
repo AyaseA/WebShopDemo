e59a79b406e2458370747231eaac28d573bf5f0c
wangdonggang <wang_dgang@massclouds.com>
2017-06-12 17:45:40 +0800

    1

--- wechat/theme/default/index/index_view.js
+++ wechat/theme/default/index/index_view.js
@@ -81,7 +81,25 @@ $(function(){
                         '../../plug/Pinyin.min.js',
                         function() {
                             var cityList = getSortCities(res.Data).sort(function(a, b) {
-                                return a.alpha > b.alpha;
+                                val1 = a.pyName;
+                                val2 = b.pyName;
+                                // 获取较长的拼音的长度
+                                var length =  val1.length > val2.length ? val1.length : val2.length;
+                                // 依次比较字母的unicode码，相等时返回0，小于时返回-1，大于时返回1
+                                for(var i = 0; i < length; i++ ) {
+                                    var differ = val1.charCodeAt(i) - val2.charCodeAt(i);
+                                    if(differ == 0) {
+                                        continue;
+                                    }else {
+                                        if(val1.charAt(i) == '_' ) {
+                                            return -1;
+                                        }
+                                        return differ;
+                                    }
+                                }    
+                                if(i == length) {
+                                    return val1.length - val2.length;
+                                }
                             });
                             console.log(cityList);
                         }

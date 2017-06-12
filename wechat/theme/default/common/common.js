f5078aa50c25016aba42fae543d1f8bc03ddc2c7
wangdonggang <wang_dgang@massclouds.com>
2017-06-12 17:01:39 +0800

    wechat config

--- wechat/theme/default/common/common.js
+++ wechat/theme/default/common/common.js
@@ -419,32 +419,22 @@
 
     // 配置微信
     !(function() {
-        /*$$.get(
+        $$.get(
             'Product/WeChat/GetSign?url=' + escape(location.href),
             function(res) {
-                console.log(res);
                 if (wx) {
                     wx.config({
-                         debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                         appId: 'wx2c53034422e377cc', // 必填，公众号的唯一标识
-                         timestamp: res.timestamp, // 必填，生成签名的时间戳
-                         nonceStr: res.noncestr, // 必填，生成签名的随机串
-                         signature: res.sign, // 必填，签名，见附录1
-                         jsApiList: ['getLocation'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-                    });
-                    wx.getLocation({
-                        type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-                        success: function (res) {
-                            var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-                            var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-                            alert(latitude + '-' + longitude);
-                            var speed = res.speed; // 速度，以米/每秒计
-                            var accuracy = res.accuracy; // 位置精度
-                        }
+                        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                        appId: 'wx2c53034422e377cc', // 必填，公众号的唯一标识
+                        timestamp: res.timestamp, // 必填，生成签名的时间戳
+                        nonceStr: res.noncestr, // 必填，生成签名的随机串
+                        signature: res.sign, // 必填，签名，见附录1
+                        jsApiList: ['getLocation','onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareQZone']
+ // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
                     });
                 }
             }
-        );*/
+        );
     }());
 
     win.$$ = $$;

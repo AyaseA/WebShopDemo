f655919277027fe4030fe6171b54db02d7f85016
wangdonggang <wang_dgang@massclouds.com>
2017-06-12 18:28:49 +0800

    1

--- wechat/theme/default/common/common.js
+++ wechat/theme/default/common/common.js
@@ -424,7 +424,7 @@
             function(res) {
                 if (wx) {
                     wx.config({
-                        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                         appId: 'wx2c53034422e377cc', // 必填，公众号的唯一标识
                         timestamp: res.timestamp, // 必填，生成签名的时间戳
                         nonceStr: res.noncestr, // 必填，生成签名的随机串

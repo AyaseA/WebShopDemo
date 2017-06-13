b7def45b3438f9e8cb3244fb806024a1c4dc5189
yangcunya <769788980@qq.com>
2017-06-13 16:57:28 +0800

    1

--- wechat/theme/default/logout/logout_view.js
+++ wechat/theme/default/logout/logout_view.js
@@ -10,6 +10,11 @@
 	$('.logout_right').on('click', function(){
 		$$.redirect('login/login.html');
 		$$.delCookie('__TOKEN__');
+		$('.logout_but_out').css('display', 'none');
+		$('.logout_but_words').css('display', 'none')
+		$('.logout_prot_tips').css('display', 'none');
+		$('.logout_right').css('display', 'none');
+		$('.logout_wrong').css('display', 'none');		
 	});
 	$('.logout_wrong').on('click', function(){
 		$('.logout_but_out').css('display', 'none');

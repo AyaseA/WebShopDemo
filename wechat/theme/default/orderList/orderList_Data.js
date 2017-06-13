cfcdda586747ecd38f9ef891b86b2abd6cedfe49
liyiheng <864686776@qq.com>
2017-06-13 14:49:02 +0800

    1

--- wechat/theme/default/orderList/orderList_Data.js
+++ wechat/theme/default/orderList/orderList_Data.js
@@ -4,24 +4,46 @@ $(function() {
 
     var contentHeight = $page.height() - $page.find(".nav").height() - $page.find(".header").height();
     $page.find(".content").css("height", contentHeight + "px");
+    $page.find(".all").css("height", contentHeight + "px");
+    $page.find(".waitPay").css("height", contentHeight + "px");
+    $page.find(".waitPost").css("height", contentHeight + "px");
+    $page.find(".waitGet").css("height", contentHeight + "px");
+    $page.find(".waitRevice").css("height", contentHeight + "px");
 
     //var Token = "eyJVc2VySUQiOiIxMCIsIk5pY2tOYW1lIjpudWxsLCJHcm91dGhWYWx1ZSI6bnVsbCwiVXNlckFkZHJlc3NJRCI6bnVsbCwiQWRkVGltZSI6IjE0OTYyODA2NDUiLCJVc2VyQ2FySUQiOm51bGwsIkltZyI6bnVsbCwiRW5hYmxlIjoiMSIsIk1vYmlsZSI6IjE1MDY2NjcwMzIwIiwiU2Vzc2lvbklEIjoiMSIsIlR5cGUiOiJVc2VyIiwiVUlEIjoiNWEzYzY5YWYwOWU4ZDA1ODBlN2QwZTdjZTY1NjdlMWUifQ%3D%3D";
     var Token = $$.getToken();
 
     //$$.post("http://192.168.1.110:8000/CSL/Order/QueryOrderList",{},)
         
+    //设置全局参数
+    var n = 1,
+    isLoad = 0,
+    noDate = 0,
+    
+    thisLoaded = {
+        all:0,
+        waitPay:0,
+        waitPost:0,
+        waitGet:0,
+        waitRevice:0,
+    },
 
-    var n = 1;
-    var isLoad = 0;
-    var noDate = 0;
+    haveLoad = {
+        all:0,
+        waitPay:0,
+        waitPost:0,
+        waitGet:0,
+        waitRevice:0,
+    };
+
     //底部加载
-    loadList({ "Token": Token, "N": n }, ".all");
+    loadList({ "Token": Token, "N": n }, ".all","all");
     
     //点击支付按钮事件  
     $page.off("click", ".PayBtn").on("click", ".PayBtn", function() {
         var orderId = $(this).parent().parent().children(".pieceHeader").children(".orderID").children().html();
         if ($(this).html() == "支付") {
-            $$.redirect("orderCommit/orderCommit.html?oid=" + orderId);
+            $$.redirect("payCenter/payCenter.html?oid=" + orderId);
         } else if ($(this).html() == "确定收货") {
             $$.redirect("commit/commit.html?oid=" + orderId);
         }
@@ -35,18 +57,21 @@ $(function() {
     });
 
     //进入主界面加载数据
-    function loadList(postData, area) {
+    function loadList(postData, area, scrollArea) {
         var pageNum = postData.N || 1;
         $.ajax({
             type: "POST",
             url: "http://192.168.1.110:8000/CSL/Order/QueryOrderList",
             data: postData,
             success: function(data) {
+                if(thisLoaded[scrollArea] == 0){
                 data = $$.eval(data);
+               
                 if(data.Status != 0){
                     console.log(data.Status);
                     alert("服务有问题");
                 }
+
                 var list = $$.eval(data).Data.Rows;
                 $page.listNum = list.length;
                 for (var i = 0; i < list.length; i++) {
@@ -99,13 +124,27 @@ $(function() {
                         }
                     }
 
+                    }
                 }
 
-                $("#orderList_orderList .content").scroll(function() {
-                    var scrollTop = $page.find(".content").scrollTop() + $(".content").height();
-                    var scrollHeight = $page.find(".content")[0].scrollHeight;
+
+
+                if(scrollArea == "all"){
+                    thisLoaded.all=1;
+                }else if(scrollArea == "waitPay"){
+                    thisLoaded.waitPay=1;
+                }else if(scrollArea == "waitPost"){
+                    thisLoaded.waitPost=1;           
+                }else if(scrollArea == "waitGet"){
+                    thisLoaded.waitGet=1;
+                }
+
+                console.log(thisLoaded);
+                $("#orderList_orderList ."+scrollArea).scroll(function() {
+                    var scrollTop = $page.find("."+scrollArea).scrollTop() + $("."+scrollArea).height();
+                    var scrollHeight = $page.find("."+scrollArea)[0].scrollHeight;
                     if (noDate == 0) {
-                        if (scrollHeight - scrollTop <= 1) {
+                        if (scrollHeight - scrollTop <= 2) {
                             if (isLoad == 0) {
                                 $.ajax({
                                     type: "POST",
@@ -180,12 +219,21 @@ $(function() {
 
                                 });
                             }
-
+                            //全部加载完成
                             if ($page.listNum < 10) {
                                 isLoad = 1;
                                 var loadMsg = "<div class='loadMsg'>已加载全部订单<div>";
-                                $page.find(".content").append(loadMsg);
+                                $page.find("."+scrollArea).append(loadMsg);
                                 noDate = 1;
+                                if(scrollArea == "all"){
+                                    haveLoad.all=1;
+                                }else if(scrollArea == "waitPay"){
+                                    haveLoad.waitPay=1;
+                                }else if(scrollArea == "waitPost"){
+                                    haveLoad.waitPost=1;           
+                                }else if(scrollArea == "waitGet"){
+                                    haveLoad.waitGet=1;
+                                }
                             }
                         }
                     }
@@ -199,56 +247,65 @@ $(function() {
     //点击全部事件
     $page.off("click", ".allNav").on("click", ".allNav", function() {
         //重置参数
-        n = 1;
-        isLoad = 0;
-        noDate = 0;
         $page.find(".nav ul li").removeClass("on");
-        $(this).addClass("on");
-        $page.find(".content div").hide();
+        $(this).addClass("on");  
+        $page.find(".content").children().hide();
         $page.find(".all").fadeIn(500);
-        loadList({ "Token": Token, "N": n }, ".all");
+        if(haveLoad.all == 0){          
+            n = 1;
+            isLoad = 0;
+            noDate = 0;
+            loadList({ "Token": Token, "N": n }, ".all", "all");
+        }
 
     });
 
     //点击待付款事件
     $page.off("click", ".waitPayNav").on("click", ".waitPayNav", function() {
-        //重置参数
-        n = 1;
-        isLoad = 0;
-        noDate = 0;
         $page.find(".nav ul li").removeClass("on");
         $(this).addClass("on");
-        $page.find(".content div").hide();
+        $page.find(".content").children().hide();
         $page.find(".waitPay").fadeIn(500);
-        loadList({ "Token": Token, "N": n, "StatusID": 1 }, ".waitPay");
-
+         $page.find(".waitPay").fadeIn(500);
+        //重置参数
+        if(haveLoad.waitPay == 0){
+            n = 1;
+            isLoad = 0;
+            noDate = 0;
+            loadList({ "Token": Token, "N": n, "StatusID": 1 }, ".waitPay", "waitPay");
+        }
     });
 
     //点击待发货事件
     $page.off("click", ".waitPostNav").on("click", ".waitPostNav", function() {
         //重置参数
-        n = 1;
-        isLoad = 0;
-        noDate = 0;
         $page.find(".nav ul li").removeClass("on");
         $(this).addClass("on");
-        $page.find(".content div").hide();
+        $page.find(".content").children().hide();
         $page.find(".waitPost").fadeIn(500);
-        loadList({ "Token": Token, "N": n, "StatusID": 3 }, ".waitPost");
+        if(haveLoad.waitPost == 0){            
+            n = 1;
+            isLoad = 0;
+            noDate = 0;
+            loadList({ "Token": Token, "N": n, "StatusID": 3 }, ".waitPost", "waitPost");
+        }
 
     });
 
     //点击待收货
     $page.off("click", ".waitGetNav").on("click", ".waitGetNav", function() {
         //重置参数
-        n = 1;
-        isLoad = 0;
-        noDate = 0;
         $page.find(".nav ul li").removeClass("on");
         $(this).addClass("on");
-        $page.find(".content div").hide();
+        $page.find(".content").children().hide();
         $page.find(".waitGet").fadeIn(500);
-        loadList({ "Token": Token, "N": n, "StatusID": 4 }, ".waitGet");
+        console.log(haveLoad.waitGet);
+        if(haveLoad.waitGet == 0){
+            n = 1;
+            isLoad = 0;
+            noDate = 0;
+            loadList({ "Token": Token, "N": n, "StatusID": 4 }, ".waitGet", "waitGet");
+        }
 
     });
 
@@ -256,15 +313,31 @@ $(function() {
     $page.off("click", ".waitReviceNav").on("click", ".waitReviceNav", function() {
         $page.find(".nav ul li").removeClass("on");
         $(this).addClass("on");
-        $page.find(".content div").hide();
+        $page.find(".content").children().hide();
         $page.find(".waitRevice").fadeIn(500);
+        productInfo = "";
         $.ajax({
             type:"POST",
             url:"http://192.168.1.110:8000/CSL/Review/ProductReviewList",
             data:{"Token":Token,IsReview:0},
-            success:function(Data){
+            success:function (Data){
                 Data=$$.eval(Data);
                 console.log(Data);
+                var list = Data.Data.Rows;
+                console.log(list);
+                for(var i = 0;i < list.length;i++){
+                    $.ajax({
+                        type:"POST",
+                        url:"http://192.168.1.110:8000/Product/Prod/QueryDetail",
+                        data:{"ID":list[i].ProductID},
+                        success:function (Data){
+                            productInfo = $$.eval(Data).Data;   
+                        }
+                    });
+                    console.log(productInfo);
+                    onePiece = onePiece = "<div class='onePiece'><div class='pieceHeader'><span class='orderID'>订单号：<span style='color:red'>" + list[i].OrderID + "</span><p class='pieceStatus'>订单完成</p></div><div class='pieceContent'><div><div class='pInfo'><p>商品名称:<span>"+productInfo.Name+"</span></p><p>商品单价:<span>"+productInfo.Price+"</span></p></div></div></div><div class='piecePay'><button class='PayBtn'>评论</button></div></div>";
+                    $page.find(".waitRevice").append(onePiece);  
+                }
             },
         });
 

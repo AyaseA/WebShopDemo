2e359d58fdf060ca58bae68ccd1b079376ea862c
wangdonggang <wang_dgang@massclouds.com>
2017-06-13 14:43:18 +0800

    修复返回跳转两次推荐保养页面的bug

--- wechat/theme/default/maintain/maintain_view.js
+++ wechat/theme/default/maintain/maintain_view.js
@@ -2,14 +2,18 @@ $(function() {
     var bodyHeight = window.innerHeight || document.body.clientHeight,
     	$page = $('#maintain_maintain'),
 	    pageStr = 'maintain_maintain';
-	
+	// 设置高度
 	$page.find('div.products').height(
 		bodyHeight - 81
 	);
-	
 	// 设置返回按钮可用
     $$.setGoBack($page.find('>div.header >a.goBack'));
-    
+    // 修改车辆事件
+    $page.on('click', '>div.header >a.modify', function() {
+        $$.redirect('myCars/myCars.html', {
+            fromGoBack: true
+        });
+    });
     // 点击跳转到商品详情
     $page.on('click', 'div.products >div.item', function() {
         var pid = $(this).attr('data-id');

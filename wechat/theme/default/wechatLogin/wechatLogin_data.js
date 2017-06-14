ff2e1c883439cb7927f2fe1b6d4bbd126683c4f6
lc <617650056@qq.com>
2017-06-14 14:45:16 +0800

    weixinlogin

--- wechat/theme/default/wechatLogin/wechatLogin_data.js
+++ wechat/theme/default/wechatLogin/wechatLogin_data.js
@@ -1,9 +1,9 @@
 $(function() {
     // var $page = $('#wechatLogin_wechatLogin'),
     // pageStr = 'wechatLogin_wechatLogin';
-    
+
     // var phoneNum = $page.find('#login_phone_num');
     // var verify = $page.find('#login_verify');
 
-  
+    $$.config.hideGlobalMenu();
 });

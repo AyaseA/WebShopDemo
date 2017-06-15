1821f47914a2aa44f9a3d37b05bd42e77d0c34ac
lc <617650056@qq.com>
2017-06-15 15:45:00 +0800

    微信签名配置

--- wechat/theme/default/share/share_data.js
+++ wechat/theme/default/share/share_data.js
@@ -1,16 +1,19 @@
 $(function() {
 
 
-    // var getSignUrl = 'Product/WeChat/GetSign?url=' + window.location + 'share/share.html';
+    
 
-    // wx.checkJsApi({
-    //     jsApiList: ['onMenuShareAppMessage'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
-    //     success: function(res) {
-    //         alert('123123');
-    //         // 以键值对的形式返回，可用的api值true，不可用为false
-    //         // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
-    //     }
-    // });
+    var weChatSign = $$.getWeChatSign();
+
+    wx.config({
+        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        appId: 'wx2c53034422e377cc', // 必填，公众号的唯一标识
+        timestamp: weChatSign.timestamp, // 必填，生成签名的时间戳
+        nonceStr: weChatSign.noncestr, // 必填，生成签名的随机串
+        signature: weChatSign.sign, // 必填，签名，见附录1
+        jsApiList: ['getLocation', 'onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareQZone']
+            // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+    });
 
 
     // -- 4. 通过ready接口处理成功验证

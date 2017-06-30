af927c5fbca2cab35fbbe8f44f46d10214210fef
yangcunya <769788980@qq.com>
2017-06-30 14:53:59 +0800

    1

--- wechat/theme/default/information/information_data.js
+++ wechat/theme/default/information/information_data.js
@@ -6,7 +6,7 @@ $('#information_information .img_icon').attr('src', imgUrl + resImg.Img);
 //微信配置
 var WXsign = $$.getWeChatSign();
 wx.config({
-    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: 'wx2c53034422e377cc', // 必填，公众号的唯一标识
     timestamp: WXsign.timestamp, // 必填，生成签名的时间戳
     nonceStr: WXsign.noncestr, // 必填，生成签名的随机串
@@ -21,7 +21,7 @@ wx.ready(function() {
 
 wx.error(function(res) {
     alert("接口处理失败");
-    alert(res);
+    console.log(res);
     // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
 });
 

93e1f9f8cf5373ee0977e071a15f81d08fbf8d16
yangcunya <769788980@qq.com>
2017-06-30 14:44:57 +0800

    1

--- wechat/theme/default/information/information_data.js
+++ wechat/theme/default/information/information_data.js
@@ -20,7 +20,7 @@ wx.ready(function() {
 });
 
 wx.error(function(res) {
-    console.log("接口处理失败");
+    alert("接口处理失败");
     console.log(res);
     // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
 });
@@ -34,12 +34,14 @@ wx.error(function(res) {
 //拍照调取本地相机和相册  
 $('.photo_take').click(function() {
 	var token = $$.getToken();
+    alert(1);
     wx.chooseImage({
         count: 1, // 默认9
         sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
         sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
         success: function(res) {
             //  res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片;
+            alert(2);
             //预览
             $('.img_icon').on('click', function() {
                 wx.previewImage({
@@ -65,12 +67,13 @@ $('.photo_take').click(function() {
 //调取微信头像
 $('.photo_photogragh').click(function(){
     $$.post(url + '/Product/WeChat/GetWUserInfo',{Url: 'information/information.html',Status: 0},function(res){
-        var chars = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-        var res = '';
+
+       /* var chars = ['0','1','2','3','4','5','6','7','8','9',
+
         for (var i = 3; i > 0; i--) {
             var id = Math.ceil(Math.random() * 35);
             res += chars[id];
-        };
-        window.open(res.Data+ '?' + res);
+        };*/
+        window.open(res.Data);
     });
 });

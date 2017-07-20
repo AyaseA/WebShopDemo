af44e11142a9c525121756356d9450313ec92d43
liyiheng <864686776@qq.com>
2017-07-20 16:35:08 +0800

    1

--- wechat/web/main/icenter/myWallet/myWallet_data.js
+++ wechat/web/main/icenter/myWallet/myWallet_data.js
@@ -7,35 +7,17 @@ $(function() {
         var url = $$.config.serverAddr;
         var token = $$.getToken();
         
-        $.ajax({
-            type: "POST",
-            url: url + "CSL/Account/QueryAccountCount",
-            data: { "WToken": token },
-            success: function(txt) {
-                txt = $$.eval(txt);
-                if (txt.Status != 0) {
-                    alert("接口错误");
-                } else {
-                    var data = txt.Data;
-                    $page.find(".myMoney").html(data);
+        $$.post("http://api.cheshili.com.cn/CSL/User/QueryICenterInfo",
+            {},
+            function(txt){
+                if(txt.Status==0){
+                    var info=txt.Data;
+                    $page.find(".myReward").html(info.RewardBalance);
+                    $page.find(".myVoucher").html(info.VoucherCount);
+                    $page.find(".myMoney").html(info.Balance);
                 }
             }
-        });
-
-        $.ajax({
-            type: "POST",
-            url: url + "CSL/Reward/QueryRewardCount",
-            data: { "WToken": token },
-            success: function(txt) {
-                txt = $$.eval(txt);
-                if (txt.Status != 0) {
-                    alert("接口错误");
-                } else {
-                    var data = txt.Data;
-                    $page.find(".myReward").html(data);
-                }
-            }
-        });
+        );
     }
 
 });

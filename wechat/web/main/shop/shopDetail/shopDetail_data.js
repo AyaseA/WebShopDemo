e11eb9fce876adf1675eeeb275d25e953a805a71
liyiheng <864686776@qq.com>
2017-07-21 10:05:21 +0800

    1

--- wechat/web/main/shop/shopDetail/shopDetail_data.js
+++ wechat/web/main/shop/shopDetail/shopDetail_data.js
@@ -38,7 +38,7 @@ $(function() {
     );
 
 
-    $page.on("click", ".btn", function() {
+    $page.off(".btn").on("click", ".btn", function() {
         if ($(this).attr("data-watch") == 0) {
             $$.post("http://api.cheshili.com.cn/CSL/StoreFollow/AddUpdateFollow", { StoreID: id },
                 function(txt) {

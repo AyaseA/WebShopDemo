5ffb02668ad04f56232987175e2d8f0c4276d2ae
wangdonggang <wang_dgang@massclouds.com>
2017-07-23 17:08:14 +0800

    1

--- wechat/web/main/icenter/orderDetail/orderDetail_view.js
+++ wechat/web/main/icenter/orderDetail/orderDetail_view.js
@@ -10,7 +10,9 @@ $(function () {
     );
     // 设置返回按钮可用
     $page.on('click', '>div.header >a.goBack', function() {
-        $$.redirect('icenter/orderList.html');
+        $$.redirect('icenter/orderList.html', {
+            fromGoBack: true
+        });
     });
 
 });

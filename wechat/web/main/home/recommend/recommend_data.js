cee73aeb2c522b19c2b42380c7db5dfc9af143a0
wangdonggang <wang_dgang@massclouds.com>
2017-07-27 10:16:58 +0800

    11

--- wechat/web/main/home/recommend/recommend_data.js
+++ wechat/web/main/home/recommend/recommend_data.js
@@ -6,9 +6,8 @@ $(function () {
         var qrCode = $$.getCookie('__RECOMMENDQR__'),
             mobile = $$.getUserMobile(),
             registerCont = $$.getUserInfo().InviteCode,
-            shareLink = location.href.split('?')[0] +
-                "?R=home/wechatLogin.html&RegisterFrom=1&RegisterCont=" +
-                registerCont;
+            shareLink = location.href.split('?')[0] + '?R=' +
+                escape('home/wechatLogin.html?RegisterFrom=1&RegisterCont=' + registerCont);
         $page.find('>div.main >div.content >span').text(mobile);
         $('#home_recommend_qrcode').empty();
         new QRCode($('#home_recommend_qrcode')[0], {

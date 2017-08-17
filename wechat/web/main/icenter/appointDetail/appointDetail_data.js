2631d4300e21d3a2e447e485b8d7ef13da19d38c
wangdonggang <wang_dgang@massclouds.com>
2017-08-17 10:49:10 +0800

    1

---
+++ wechat/web/main/icenter/appointDetail/appointDetail_data.js
@@ -0,0 +1,121 @@
+$(function () {
+    var bodyHeight = window.innerHeight || document.body.clientHeight,
+        $page = $('#icenter_appointDetail'),
+        pageStr = 'icenter_appointDetail',
+        headerHeight = $page.find('>div.header').height(),
+        appointId = $$.getQueryString('aid'),
+        appointStatus = $$.getQueryString('status') || 0;
+
+    $page.off('click', 'div.product a.contactService').on('click','div.product a.contactService', function() {
+        $page.find('div.confirm').show();
+    });
+    $page.off('click', 'div.confirm, div.confirm button.cancel')
+        .on('click', 'div.confirm, div.confirm button.cancel', function() {
+        $page.find('div.confirm').hide();
+    });
+    $page.off('click', 'div.confirm >div').on('click', 'div.confirm >div', function(e) {
+        e.stopPropagation();
+    });
+    $page.off('click', 'button.cancelOrder').on('click', 'button.cancelOrder', function() {
+        layer.confirm('确认取消订单？', { icon: 3, title: '提示' }, function(index) {
+            cancelOrder();
+            layer.close(index);
+        });
+    });
+    $page.off('click', '>div.footer >a').on('click', '>div.footer >a', function() {
+        makeCode();
+    });
+    // 获取订单详情
+	getAppointDetail();
+	function getAppointDetail() {
+		$$.post(
+			'CSL/Appointment/QueryServiceAppointDetail',
+			{
+				'ID': appointId
+			},
+			function(res) {
+				if (res.Status != 0) {
+                    return false;
+                }
+                if (res.Data) {
+                	var d = res.Data;
+                	var hours = $$.timeToStr(d.AppointTimeS, 'HH'),
+                        amOrPm;
+
+                    if (hours > 12) {
+                        amOrPm = '下午';
+                    } else {
+                        amOrPm = '上午';
+                    }
+
+                    $page.find('>div.main').html(template(pageStr + '_detail', {
+                        serverAddr: $$.config.serverAddr,
+                        data: d,
+                        appointStatus: appointStatus,
+                        appointTime: $$.timeToStr(d.AppointDate) + ' ' + amOrPm,
+                        discount: parseFloat(d.AllMoney - d.OrderOutPocket).toFixed(2)
+                    }));
+
+                    $page.find('>div.footer').html(template(pageStr + '_footer', {
+                        appointStatus: appointStatus
+                    }));
+                }
+			}
+		);
+	}
+    //未预约
+    function makeCode() {
+        var imgurl = $$.serverAddr + "CSL/Service/QueryMyServiceImgByAID?ID=" + appointId + "&WToken=" + $$.getToken();
+        console.log(imgurl)
+        var codeTime = 120;
+        var checkTime = 0;
+        time = setInterval(function() {
+            codeTime -= 1;
+            checkTime += 1;
+
+            if (checkTime < 15) {
+                checkServiceCode(appointId);
+            } else if (checkTime > 15 && checkTime < 30) {
+                if (checkTime % 2 == 0) {
+                    checkServiceCode(appointId);
+                }
+            } else if (checkTime > 30 && checkTime < 60) {
+                if (checkTime % 3 == 0) {
+                    checkServiceCode(appointId);
+                }
+            } else {
+                if (checkTime % 4 == 0) {
+                    checkServiceCode(appointId);
+                }
+            }
+
+            if (codeTime == 5) {
+                changeImgurl = $$.config.serverAddr + "CSL/Service/QueryMyServiceImgByAID?ID=" + appointId + "&WToken=" + token + "&t=" + Math.random();
+                codeTime = 120;
+                $(".appointment_strCode").attr("src", changeImgurl);
+            }
+        }, 1000);
+        layer.open({
+            title: "商家扫描二维码",
+            content: "<img style='width:59vw;' class='appointment_strCode' src='" + imgurl + "'>",
+            area: ["50vw", "80vw"],
+            btn: [],
+            cancel: function() {
+                clearInterval(time);
+            }
+        });
+    }
+
+    function checkServiceCode(sid) {
+        $$.post("CSL/Service/ConfirmMyService", {
+                ID: sid
+            },
+            function(txt) {
+                if (txt.Status == 0) {
+                    layer.msg("二维码已被成功扫描");
+                    clearInterval(time);
+                }
+            }
+        );
+    }
+});
---
+++ wechat/web/main/icenter/appointDetail/appointDetail_view.js
@@ -0,0 +1,18 @@
+$(function () {
+    var bodyHeight = window.innerHeight || document.body.clientHeight,
+        $page = $('#icenter_appointDetail'),
+        pageStr = 'icenter_appointDetail',
+        headerHeight = $page.find('>div.header').height(),
+        footerHeight = $page.find('>div.footer').height();
+    // 设置界面高度
+    $page.find('>div.main').height(
+        bodyHeight - headerHeight - footerHeight - 1
+    );
+    // 设置返回按钮可用
+    $page.on('click', '>div.header >a.goBack', function() {
+        $$.redirect('icenter/orderList.html', {
+            fromGoBack: true
+        });
+    });
+
+});

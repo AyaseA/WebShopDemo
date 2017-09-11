ba0efb5b11633b056a32c0229f5c20db7816ee91
wangdonggang <wang_dgang@massclouds.com>
2017-09-11 09:05:28 +0800

    1

--- wechat/web/main/icenter/contact/contact_data.js
+++ wechat/web/main/icenter/contact/contact_data.js
@@ -3,42 +3,44 @@ $(function () {
         pageStr = 'icenter_contact',
         callCode = $$.getQueryString('CallCode');
     
-    $$.post('CSL/P_Msg/TriCall', {
-        CallCode: callCode
-    }, function(res) {
-        if (res.Status == 0) {
-            $page.find('>div.main').show();
-            $page.find('>div.check').hide();
-        } else if (res.Status == 95) {
-            layer.msg('该二维码未绑定用户');
-            $$.goBack();
-        } else if (res.Status == 96) {
-            $page.find('>div.main').hide();
-            $page.find('>div.check').show();
-        } else if (res.Status == 97) {
-            layer.msg('不能给自己打电话');
-            $$.goBack();
-        } else if (res.Status == 98) {
-            layer.msg('二维码不可用');
-            $$.goBack();
-        } else if (res.Status == 99) {
-            layer.msg('拨打的号码不存在');
-            $$.goBack();
-        } else if (res.Status == 999) {
-            layer.msg('未知错误');
-            $$.goBack();
-        }
-    });
+    if ($$.isLogin(true)) {
+        $$.post('CSL/P_Msg/TriCall', {
+            CallCode: callCode
+        }, function(res) {
+            if (res.Status == 0) {
+                $page.find('>div.main').show();
+                $page.find('>div.check').hide();
+            } else if (res.Status == 95) {
+                layer.msg('该二维码未绑定用户');
+                $$.goBack();
+            } else if (res.Status == 96) {
+                $page.find('>div.main').hide();
+                $page.find('>div.check').show();
+            } else if (res.Status == 97) {
+                layer.msg('不能给自己打电话');
+                $$.goBack();
+            } else if (res.Status == 98) {
+                layer.msg('二维码不可用');
+                $$.goBack();
+            } else if (res.Status == 99) {
+                layer.msg('拨打的号码不存在');
+                $$.goBack();
+            } else if (res.Status == 999) {
+                layer.msg('未知错误');
+                $$.goBack();
+            }
+        });
 
-    $page.off('click dbclick', '>div.check >p >span, >div.check >a')
-         .on('click dbclick', '>div.check >p >span, >div.check >a', function() {
-        $$.redirect('home/fillOrder.html?pid=48&type=0&callcode=' + callCode);
-    });
+        $page.off('click dbclick', '>div.check >p >span, >div.check >a')
+             .on('click dbclick', '>div.check >p >span, >div.check >a', function() {
+            $$.redirect('home/fillOrder.html?pid=48&type=0&callcode=' + callCode);
+        });
 
-    $$.config.hideGlobalMenu();
-    
-    $page.find('div.warp').html(template(pageStr + '_content', {
-        type: 'msg',
-        callCode: callCode
-    }));
+        $$.config.hideGlobalMenu();
+        
+        $page.find('div.warp').html(template(pageStr + '_content', {
+            type: 'msg',
+            callCode: callCode
+        }));
+    }
 });

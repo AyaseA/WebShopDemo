13153e03a69e3ae9638f74d0193da0f9f3523ea6
yangcunya <769788980@qq.com>
2017-10-19 11:34:39 +0800

    修改

--- wechat/web/main/icenter/orderDetail/orderDetail_view.js
+++ wechat/web/main/icenter/orderDetail/orderDetail_view.js
@@ -4,6 +4,9 @@ $(function () {
         pageStr = 'icenter_orderDetail',
         headerHeight = $page.find('>div.header').height(),
         footerHeight = $page.find('>div.footer').height();
-        
+
+    $page.find('div.header >a').click(function(){
+        $$.goBack();
+    });
 
 });

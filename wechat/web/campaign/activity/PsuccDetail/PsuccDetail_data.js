e6c2aa7ae6126c1149e715e2ebe9638705e13cd1
yangcunya <769788980@qq.com>
2017-10-24 18:41:49 +0800

    修改

--- wechat/web/campaign/activity/PsuccDetail/PsuccDetail_data.js
+++ wechat/web/campaign/activity/PsuccDetail/PsuccDetail_data.js
@@ -2,14 +2,7 @@ $(function(){
     var $page=$("#activity_PsuccDetail"),
         $pagestr="activity_PsuccDetail";
 
-//计算div.main 的高度
-    function confirmMainHeight(){
-        var  headerHeight=$page.find("div.header").height(),
-            footerHeight=$page.find("div.footer").height(),
-            bodyHeight = window.innerHeight || document.body.clientHeight;
-        $page.find("div.main").height(bodyHeight-footerHeight-headerHeight);
-    }
-    confirmMainHeight();
+
 
 
 
--- wechat/web/campaign/activity/PsuccDetail/PsuccDetail_view.js
+++ wechat/web/campaign/activity/PsuccDetail/PsuccDetail_view.js
@@ -5,7 +5,7 @@ $(function(){
 //点击返回按钮
     $page.off("click dblclick" , "div.header a.goBack").on("click", "div.header a.goBack" , function(){
         $$.goBack();
-    })
+    });
     //模态框的弹出
     $page.on("click","div.footer button",function(){
         $page.find("div.Pmodel").fadeIn(1000);
@@ -17,4 +17,12 @@ $(function(){
             clearTimeout(timer);
         },5000)
     }
+    //计算div.main 的高度
+    function confirmMainHeight(){
+        var  headerHeight=$page.find("div.header").height(),
+            footerHeight=$page.find("div.footer").height(),
+            bodyHeight = window.innerHeight || document.body.clientHeight;
+        $page.find("div.main").height(bodyHeight-footerHeight-headerHeight);
+    }
+    confirmMainHeight();
 })

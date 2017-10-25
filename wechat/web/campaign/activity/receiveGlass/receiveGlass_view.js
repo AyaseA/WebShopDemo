6e28f70fcb461a9acdd8820fba284bbb6eb97934
yangcunya <769788980@qq.com>
2017-10-25 14:57:27 +0800

    修改

--- wechat/web/campaign/activity/receiveGlass/receiveGlass_view.js
+++ wechat/web/campaign/activity/receiveGlass/receiveGlass_view.js
@@ -9,10 +9,28 @@ $(function(){
             if(!(/^1[3|4|5|8][0-9]\d{4,8}$/.test(Mobile))){
                 layer.msg("请输入正取手机号");
                 return false;
+            }else{
+                //判断是否有着手机号
+                $.ajax({
+                    url:$$.serverAddr+"CSL/Login/HasUserMobile",
+                    data:{Mobile:Mobile},
+                    type: 'POST',
+                    success:function(res){
+                        res=JSON.parse(res);
+                        if(res.Status == 0){//手机号登录过
+                            $$.post("")
+
+                        }else{//没有登录，跳转到注册页面
+                            $$.redirect("activity/glassLogin.html");
+                        }
+                    }
+                })
+
             }
         }else{
             layer.msg("请输入手机号");
         }
-    })
+    });
+
 
 })
